d8c2c9d7cc8437dca52cb346eca3c5b436cbe2d3
Yuchen Liu <liu2529508462@gmail.com>
2020-02-16 17:35:32 -0500

    implement myMatch to both train and test labels

--- wineLeastSquare.m
+++ wineLeastSquare.m
@@ -3,7 +3,9 @@ dataset = 'wine';
 [train_featureVector, train_labels, test_featureVector, test_labels] = loadDataset(dataset);
 numGroups = length(countcats(test_labels));
 
-test_labels = myMatch(dataset,test_labels); % map labels to double values
+% map labels to double values
+train_labels = myMatch(dataset,train_labels);
+test_labels = myMatch(dataset,test_labels); 
 
 feature_idx = 1:size(train_featureVector,2);
 
@@ -17,9 +19,9 @@ X = [ones(length(train_featureVector),1) train_featureVector];
 T = zeros(length(train_featureVector),numGroups);
 
 for i=1:length(train_labels)
-   if train_labels(i,1) == '1'
+   if train_labels(i,1) == 1
        T(i,:) = [1 0 0];
-   elseif train_labels(i,1) == '2'
+   elseif train_labels(i,1) == 2
        T(i,:) = [0 1 0];
    else
        T(i,:) = [0 0 1];
